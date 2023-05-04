c8d948732d5028b64e9d1b6d0e22130ff75e7a0e
TomTom258 <01tomas01@gmail.com>
2023-05-04 13:21:21 +0200

    Git repo init

---
+++ Reddit Clone Backend/src/test/java/com/example/redditclone/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
