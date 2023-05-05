3c767d4587c97b50d66b45b59e69acbc2fdf9567
TomTom258 <01tomas01@gmail.com>
2023-05-05 09:46:25 +0200

    Added env variables

--- Reddit Clone Backend/src/main/java/com/example/redditclone/RedditCloneApplication.java
+++ Reddit Clone Backend/src/main/java/com/example/redditclone/RedditCloneApplication.java
@@ -1,12 +1,15 @@
 package com.example.redditclone;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
