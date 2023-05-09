663d4545bab00869dc9fdd7ed815c22595648c63
TomTom258 <01tomas01@gmail.com>
2023-05-09 14:14:53 +0200

    Added PostControllerTest - not passing yet

---
+++ Reddit Clone Backend/src/test/java/com/example/redditclone/posts/controllers/PostControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.redditclone.posts.controllers;
+
+import com.example.redditclone.dtos.PostDto;
+import com.example.redditclone.dtos.RegisterDto;
+import com.example.redditclone.posts.models.Post;
+import com.example.redditclone.posts.repositories.PostRepository;
+import com.example.redditclone.posts.services.PostValidator;
+import com.example.redditclone.users.controllers.AuthController;
+import com.example.redditclone.users.models.User;
+import com.example.redditclone.users.repositories.UserRepository;
+import com.example.redditclone.users.services.RegistrationValidator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(controllers = AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class PostControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private PostValidator postValidator;
+    @MockBean
+    private PostRepository postRepository;
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean
+    private RegistrationValidator registrationValidator;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void PostController_Returns_Created_When_Adding_Post() throws Exception {
+        PostDto postDto = new PostDto("testTitle", "This is the first sentence.");
+        Post newPost = new Post("testTitle", "This is the first sentence.", "testUser");
+        User fakeUser = new User("testUser", "test@test.com", "password1234", false);
+
+        Mockito.doReturn(fakeUser).when(userRepository).
+        when(postValidator.postThePost(newPost)).thenReturn(true);
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts/add")
+                        .content(objectMapper.writeValueAsString(postDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status", is(201)))
+                .andExpect(jsonPath("$.message", is("Post was successfully added")));
+    }
+}
