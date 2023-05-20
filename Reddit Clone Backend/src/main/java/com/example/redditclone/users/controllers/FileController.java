f330d73861562aa76a1d91aa793116b620139f63
TomTom258 <01tomas01@gmail.com>
2023-05-20 12:51:54 +0200

    Refactor. Cleaning unused import statements

--- Reddit Clone Backend/src/main/java/com/example/redditclone/users/controllers/FileController.java
+++ Reddit Clone Backend/src/main/java/com/example/redditclone/users/controllers/FileController.java
@@ -3,19 +3,14 @@ package com.example.redditclone.users.controllers;
 import com.example.redditclone.dtos.ErrorResponseDto;
 import com.example.redditclone.dtos.OkResponseDto;
 import com.example.redditclone.dtos.ResponseDto;
-import com.example.redditclone.users.models.User;
-import com.example.redditclone.users.repositories.UserRepository;
 import com.example.redditclone.users.services.FileService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("files")
