2fe3b696bbb291c7bc15f7387ac35a1a14255606
TomTom258 <01tomas01@gmail.com>
2023-05-24 13:20:29 +0200

    Added unit tests for Role Class

--- Reddit Clone Backend/src/test/java/com/example/redditclone/users/models/RoleTest.java
+++ Reddit Clone Backend/src/test/java/com/example/redditclone/users/models/RoleTest.java
@@ -2,34 +2,72 @@ package com.example.redditclone.users.models;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+import java.util.Set;
+
 
 class RoleTest {
-    Role role;
 
-    @BeforeEach
-    void init() {
-        role = new Role("Test Ability");
+    @Test
+    void testGettersAndSetters() {
+        // Arrange
+        String ability = "ADMIN";
+        Set<User> users = new HashSet<>();
+        Set<Permission> permissions = new HashSet<>();
+
+        // Act
+        Role role = new Role();
+        role.setAbility(ability);
+        role.setUsers(users);
+        role.setPermissions(permissions);
+
+        // Assert
+        assertEquals(null, role.getId());
+        assertEquals(ability, role.getAbility());
+        assertEquals(users, role.getUsers());
+        assertEquals(permissions, role.getPermissions());
     }
 
     @Test
-    void id_returns_null_if_instanciated() {
+    void testConstructor() {
+        // Arrange
+        String ability = "MODERATOR";
+
+        // Act
+        Role role = new Role(ability);
+
+        // Assert
         assertNull(role.getId());
-    }
-
-    @Test
-    void getAbility_returns_correct_value() {
-        assertEquals("Test Ability", role.getAbility());
-    }
-
-    @Test
-    void getUsers_returns_null_if_instanciated() {
+        assertEquals(ability, role.getAbility());
         assertEquals(null, role.getUsers());
+        assertEquals(null, role.getPermissions());
     }
 
     @Test
-    void getPermissions_returns_null_if_instanciated() {
+    void testDefaultConstructor() {
+        // Arrange & Act
+        Role role = new Role();
+
+        // Assert
+        assertNull(role.getId());
+        assertNull(role.getAbility());
+        assertEquals(null, role.getUsers());
         assertEquals(null, role.getPermissions());
+    }
+
+    @Test
+    void testSettersReturnRoleInstance() {
+        // Arrange
+        Role role = new Role();
+        Set<User> users = new HashSet<>();
+        Set<Permission> permissions = new HashSet<>();
+
+        // Act
+        Role updatedRole = role.setAbility("USER").setUsers(users).setPermissions(permissions);
+
+        // Assert
+        assertSame(role, updatedRole);
     }
 }
