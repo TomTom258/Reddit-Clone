dbe01b6a5eb894838fc17ad526aa0505e393e993
TomTom258 <01tomas01@gmail.com>
2023-06-02 11:53:54 +0200

    Slight refactor

--- Reddit Clone Backend/src/main/java/com/example/redditclone/comments/services/CommentServiceImp.java
+++ Reddit Clone Backend/src/main/java/com/example/redditclone/comments/services/CommentServiceImp.java
@@ -11,8 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
--- Reddit Clone Backend/src/main/java/com/example/redditclone/comments/services/CommentValidatorImp.java
+++ Reddit Clone Backend/src/main/java/com/example/redditclone/comments/services/CommentValidatorImp.java
@@ -12,14 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @Service
 public class CommentValidatorImp implements CommentValidator{
-
     PostRepository postRepository;
     CommentRepository commentRepository;
     UserRepository userRepository;
