39d925b2511936bf3ee6ce950557d25d6f9b5c70
TomTom258 <01tomas01@gmail.com>
2023-09-10 13:43:01 +0200

    Firts components generation. Trying out

---
+++ Frontend/Reddit/src/app/components/navbar/navbar-link/navbar-link/navbar-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar-link',
+  templateUrl: './navbar-link.component.html',
+  styleUrls: ['./navbar-link.component.css']
+})
+export class NavbarLinkComponent {
+
+}
