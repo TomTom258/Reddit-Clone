bd96b332e3891ca5629fbd103fd82ef47ee6e950
TomTom258 <01tomas01@gmail.com>
2023-09-10 13:46:43 +0200

    Left over files to commit

--- Frontend/Reddit/src/app/app.module.ts
+++ Frontend/Reddit/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarLinkComponent } from './components/navbar/navbar-link/navbar-link/navbar-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarLinkComponent,
   ],
   imports: [
     BrowserModule,
